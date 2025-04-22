6ac99947f2e56a19ac2f011540e35b408607385e
Ian Sterner <pfp_duke@yahoo.com>
2025-04-22 16:26:54 -0500

    Adding check powershell version for VScode

---
+++ checkPSV.ps1
@@ -0,0 +1,4 @@
+#ctrl+shift+p
+#change to powershell or x86 in (powershell: show session menu)
+#Windows default is currently 5.1
+Write-Host "Running in PowerShell version: $($PSVersionTable.PSVersion)"
